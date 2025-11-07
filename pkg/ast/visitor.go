3060fb7c23d2b14e30406bc0074db2b4b50a8a4d
Christian Budde <4097562+CWBudde@users.noreply.github.com>
2025-11-07 06:20:42 +0100

    feat: temp go downgrade to work on web

---
+++ pkg/ast/visitor.go
@@ -0,0 +1,639 @@
+package ast
+
+// Visitor is the interface for AST traversal using the visitor pattern.
+// Implementations should define a Visit method that is called for each node.
+// If Visit returns nil, the node's children are not traversed.
+// Otherwise, Visit is called recursively for all child nodes.
+type Visitor interface {
+	Visit(node Node) (w Visitor)
+}
+
+// Walk traverses an AST in depth-first order, starting at the given node.
+// It calls v.Visit(node) for each node encountered. If v.Visit returns nil,
+// traversal of that node's children is skipped. Otherwise, Walk is called
+// recursively for each child with the visitor returned by Visit.
+//
+// Walk follows the standard Go AST pattern from the go/ast package.
+func Walk(v Visitor, node Node) {
+	if v = v.Visit(node); v == nil {
+		return
+	}
+
+	// Walk children based on node type
+	switch n := node.(type) {
+	// Program
+	case *Program:
+		for _, stmt := range n.Statements {
+			Walk(v, stmt)
+		}
+
+	// Literals
+	case *Identifier:
+		if n.Type != nil {
+			Walk(v, n.Type)
+		}
+
+	case *IntegerLiteral:
+		if n.Type != nil {
+			Walk(v, n.Type)
+		}
+
+	case *FloatLiteral:
+		if n.Type != nil {
+			Walk(v, n.Type)
+		}
+
+	case *StringLiteral:
+		if n.Type != nil {
+			Walk(v, n.Type)
+		}
+
+	case *BooleanLiteral:
+		if n.Type != nil {
+			Walk(v, n.Type)
+		}
+
+	case *CharLiteral:
+		if n.Type != nil {
+			Walk(v, n.Type)
+		}
+
+	case *NilLiteral:
+		if n.Type != nil {
+			Walk(v, n.Type)
+		}
+
+	// Expressions
+	case *BinaryExpression:
+		Walk(v, n.Left)
+		Walk(v, n.Right)
+		if n.Type != nil {
+			Walk(v, n.Type)
+		}
+
+	case *UnaryExpression:
+		Walk(v, n.Right)
+		if n.Type != nil {
+			Walk(v, n.Type)
+		}
+
+	case *GroupedExpression:
+		Walk(v, n.Expression)
+		if n.Type != nil {
+			Walk(v, n.Type)
+		}
+
+	case *RangeExpression:
+		Walk(v, n.Start)
+		Walk(v, n.RangeEnd)
+		if n.Type != nil {
+			Walk(v, n.Type)
+		}
+
+	case *CallExpression:
+		Walk(v, n.Function)
+		for _, arg := range n.Arguments {
+			Walk(v, arg)
+		}
+		if n.Type != nil {
+			Walk(v, n.Type)
+		}
+
+	case *OldExpression:
+		Walk(v, n.Identifier)
+		if n.Type != nil {
+			Walk(v, n.Type)
+		}
+
+	// Statements
+	case *ExpressionStatement:
+		if n.Expression != nil {
+			Walk(v, n.Expression)
+		}
+
+	case *BlockStatement:
+		for _, stmt := range n.Statements {
+			Walk(v, stmt)
+		}
+
+	case *VarDeclStatement:
+		for _, name := range n.Names {
+			Walk(v, name)
+		}
+		if n.Type != nil {
+			Walk(v, n.Type)
+		}
+		if n.Value != nil {
+			Walk(v, n.Value)
+		}
+
+	case *AssignmentStatement:
+		if n.Target != nil {
+			Walk(v, n.Target)
+		}
+		if n.Value != nil {
+			Walk(v, n.Value)
+		}
+
+	case *ConstDecl:
+		Walk(v, n.Name)
+		if n.Type != nil {
+			Walk(v, n.Type)
+		}
+		Walk(v, n.Value)
+
+	// Control Flow
+	case *IfStatement:
+		Walk(v, n.Condition)
+		Walk(v, n.Consequence)
+		if n.Alternative != nil {
+			Walk(v, n.Alternative)
+		}
+
+	case *WhileStatement:
+		Walk(v, n.Condition)
+		Walk(v, n.Body)
+
+	case *RepeatStatement:
+		Walk(v, n.Body)
+		Walk(v, n.Condition)
+
+	case *ForStatement:
+		Walk(v, n.Variable)
+		Walk(v, n.Start)
+		Walk(v, n.EndValue)
+		if n.Step != nil {
+			Walk(v, n.Step)
+		}
+		Walk(v, n.Body)
+
+	case *ForInStatement:
+		Walk(v, n.Variable)
+		Walk(v, n.Collection)
+		Walk(v, n.Body)
+
+	case *CaseStatement:
+		Walk(v, n.Expression)
+		for _, branch := range n.Cases {
+			// Walk branch values and statement (CaseBranch is not a Node)
+			for _, value := range branch.Values {
+				Walk(v, value)
+			}
+			Walk(v, branch.Statement)
+		}
+		if n.Else != nil {
+			Walk(v, n.Else)
+		}
+
+	case *BreakStatement:
+		// No children
+
+	case *ContinueStatement:
+		// No children
+
+	case *ExitStatement:
+		if n.ReturnValue != nil {
+			Walk(v, n.ReturnValue)
+		}
+
+	// Functions
+	case *FunctionDecl:
+		Walk(v, n.Name)
+		if n.ClassName != nil {
+			Walk(v, n.ClassName)
+		}
+		for _, param := range n.Parameters {
+			// Walk parameter children (Parameter is not a Node)
+			if param.Name != nil {
+				Walk(v, param.Name)
+			}
+			if param.Type != nil {
+				Walk(v, param.Type)
+			}
+			if param.DefaultValue != nil {
+				Walk(v, param.DefaultValue)
+			}
+		}
+		if n.ReturnType != nil {
+			Walk(v, n.ReturnType)
+		}
+		if n.PreConditions != nil {
+			Walk(v, n.PreConditions)
+		}
+		if n.Body != nil {
+			Walk(v, n.Body)
+		}
+		if n.PostConditions != nil {
+			Walk(v, n.PostConditions)
+		}
+
+	case *ReturnStatement:
+		if n.ReturnValue != nil {
+			Walk(v, n.ReturnValue)
+		}
+
+	// Contracts
+	case *Condition:
+		if n.Test != nil {
+			Walk(v, n.Test)
+		}
+		if n.Message != nil {
+			Walk(v, n.Message)
+		}
+
+	case *PreConditions:
+		for _, cond := range n.Conditions {
+			Walk(v, cond)
+		}
+
+	case *PostConditions:
+		for _, cond := range n.Conditions {
+			Walk(v, cond)
+		}
+
+	// Arrays
+	case *ArrayDecl:
+		Walk(v, n.Name)
+		if n.ArrayType != nil {
+			Walk(v, n.ArrayType)
+		}
+
+	case *ArrayTypeAnnotation:
+		if n.LowBound != nil {
+			Walk(v, n.LowBound)
+		}
+		if n.HighBound != nil {
+			Walk(v, n.HighBound)
+		}
+		if n.ElementType != nil {
+			Walk(v, n.ElementType)
+		}
+
+	case *ArrayLiteralExpression:
+		for _, elem := range n.Elements {
+			Walk(v, elem)
+		}
+		if n.Type != nil {
+			Walk(v, n.Type)
+		}
+
+	case *IndexExpression:
+		Walk(v, n.Left)
+		Walk(v, n.Index)
+		if n.Type != nil {
+			Walk(v, n.Type)
+		}
+
+	case *NewArrayExpression:
+		if n.ElementTypeName != nil {
+			Walk(v, n.ElementTypeName)
+		}
+		for _, dim := range n.Dimensions {
+			Walk(v, dim)
+		}
+		if n.Type != nil {
+			Walk(v, n.Type)
+		}
+
+	// Classes
+	case *ClassDecl:
+		Walk(v, n.Name)
+		if n.Parent != nil {
+			Walk(v, n.Parent)
+		}
+		for _, iface := range n.Interfaces {
+			Walk(v, iface)
+		}
+		for _, field := range n.Fields {
+			Walk(v, field)
+		}
+		for _, method := range n.Methods {
+			Walk(v, method)
+		}
+		for _, operator := range n.Operators {
+			Walk(v, operator)
+		}
+		for _, prop := range n.Properties {
+			Walk(v, prop)
+		}
+		if n.Constructor != nil {
+			Walk(v, n.Constructor)
+		}
+		if n.Destructor != nil {
+			Walk(v, n.Destructor)
+		}
+
+	case *FieldDecl:
+		Walk(v, n.Name)
+		if n.Type != nil {
+			Walk(v, n.Type)
+		}
+
+	case *NewExpression:
+		Walk(v, n.ClassName)
+		for _, arg := range n.Arguments {
+			Walk(v, arg)
+		}
+		if n.Type != nil {
+			Walk(v, n.Type)
+		}
+
+	case *MemberAccessExpression:
+		Walk(v, n.Object)
+		Walk(v, n.Member)
+		if n.Type != nil {
+			Walk(v, n.Type)
+		}
+
+	case *MethodCallExpression:
+		Walk(v, n.Object)
+		Walk(v, n.Method)
+		for _, arg := range n.Arguments {
+			Walk(v, arg)
+		}
+		if n.Type != nil {
+			Walk(v, n.Type)
+		}
+
+	case *InheritedExpression:
+		if n.Method != nil {
+			Walk(v, n.Method)
+		}
+		for _, arg := range n.Arguments {
+			Walk(v, arg)
+		}
+		if n.Type != nil {
+			Walk(v, n.Type)
+		}
+
+	// Enums
+	case *EnumDecl:
+		Walk(v, n.Name)
+
+	case *EnumLiteral:
+		// No children to walk
+
+	// Records
+	case *RecordDecl:
+		Walk(v, n.Name)
+		for _, field := range n.Fields {
+			Walk(v, field)
+		}
+		for _, method := range n.Methods {
+			Walk(v, method)
+		}
+
+	case *RecordLiteralExpression:
+		if n.TypeName != nil {
+			Walk(v, n.TypeName)
+		}
+		for _, field := range n.Fields {
+			// Walk field initializer children (FieldInitializer is not a Node)
+			if field.Name != nil {
+				Walk(v, field.Name)
+			}
+			Walk(v, field.Value)
+		}
+
+	// Lambda
+	case *LambdaExpression:
+		for _, param := range n.Parameters {
+			// Walk parameter children (Parameter is not a Node)
+			if param.Name != nil {
+				Walk(v, param.Name)
+			}
+			if param.Type != nil {
+				Walk(v, param.Type)
+			}
+			if param.DefaultValue != nil {
+				Walk(v, param.DefaultValue)
+			}
+		}
+		if n.ReturnType != nil {
+			Walk(v, n.ReturnType)
+		}
+		if n.Body != nil {
+			Walk(v, n.Body)
+		}
+		if n.Type != nil {
+			Walk(v, n.Type)
+		}
+
+	// Sets
+	case *SetDecl:
+		if n.Name != nil {
+			Walk(v, n.Name)
+		}
+		if n.ElementType != nil {
+			Walk(v, n.ElementType)
+		}
+
+	case *SetLiteral:
+		for _, elem := range n.Elements {
+			Walk(v, elem)
+		}
+		if n.Type != nil {
+			Walk(v, n.Type)
+		}
+
+	// Properties
+	case *PropertyDecl:
+		Walk(v, n.Name)
+		if n.Type != nil {
+			Walk(v, n.Type)
+		}
+		for _, param := range n.IndexParams {
+			// Walk parameter children (Parameter is not a Node)
+			if param.Name != nil {
+				Walk(v, param.Name)
+			}
+			if param.Type != nil {
+				Walk(v, param.Type)
+			}
+			if param.DefaultValue != nil {
+				Walk(v, param.DefaultValue)
+			}
+		}
+		if n.ReadSpec != nil {
+			Walk(v, n.ReadSpec)
+		}
+		if n.WriteSpec != nil {
+			Walk(v, n.WriteSpec)
+		}
+
+	// Operators
+	case *OperatorDecl:
+		if n.Binding != nil {
+			Walk(v, n.Binding)
+		}
+		for _, operandType := range n.OperandTypes {
+			Walk(v, operandType)
+		}
+		if n.ReturnType != nil {
+			Walk(v, n.ReturnType)
+		}
+
+	// Exceptions
+	case *TryStatement:
+		if n.TryBlock != nil {
+			Walk(v, n.TryBlock)
+		}
+		if n.ExceptClause != nil {
+			// Walk except clause children (ExceptClause is not a Node)
+			for _, handler := range n.ExceptClause.Handlers {
+				// Walk exception handler children (ExceptionHandler is not a Node)
+				if handler.Variable != nil {
+					Walk(v, handler.Variable)
+				}
+				if handler.ExceptionType != nil {
+					Walk(v, handler.ExceptionType)
+				}
+				if handler.Statement != nil {
+					Walk(v, handler.Statement)
+				}
+			}
+			if n.ExceptClause.ElseBlock != nil {
+				Walk(v, n.ExceptClause.ElseBlock)
+			}
+		}
+		if n.FinallyClause != nil {
+			// Walk finally clause children (FinallyClause is not a Node)
+			if n.FinallyClause.Block != nil {
+				Walk(v, n.FinallyClause.Block)
+			}
+		}
+
+	case *RaiseStatement:
+		if n.Exception != nil {
+			Walk(v, n.Exception)
+		}
+
+	// Interfaces
+	case *InterfaceDecl:
+		Walk(v, n.Name)
+		if n.Parent != nil {
+			Walk(v, n.Parent)
+		}
+		for _, method := range n.Methods {
+			// Walk interface method children (InterfaceMethodDecl is not a Node)
+			Walk(v, method.Name)
+			for _, param := range method.Parameters {
+				// Walk parameter children (Parameter is not a Node)
+				if param.Name != nil {
+					Walk(v, param.Name)
+				}
+				if param.Type != nil {
+					Walk(v, param.Type)
+				}
+				if param.DefaultValue != nil {
+					Walk(v, param.DefaultValue)
+				}
+			}
+			if method.ReturnType != nil {
+				Walk(v, method.ReturnType)
+			}
+		}
+
+	// Units
+	case *UnitDeclaration:
+		Walk(v, n.Name)
+		if n.InterfaceSection != nil {
+			Walk(v, n.InterfaceSection)
+		}
+		if n.ImplementationSection != nil {
+			Walk(v, n.ImplementationSection)
+		}
+		if n.InitSection != nil {
+			Walk(v, n.InitSection)
+		}
+		if n.FinalSection != nil {
+			Walk(v, n.FinalSection)
+		}
+
+	case *UsesClause:
+		for _, unit := range n.Units {
+			Walk(v, unit)
+		}
+
+	// Type Annotations and Expressions
+	case *TypeAnnotation:
+		if n.InlineType != nil {
+			Walk(v, n.InlineType)
+		}
+
+	case *TypeDeclaration:
+		Walk(v, n.Name)
+		if n.AliasedType != nil {
+			Walk(v, n.AliasedType)
+		}
+		if n.LowBound != nil {
+			Walk(v, n.LowBound)
+		}
+		if n.HighBound != nil {
+			Walk(v, n.HighBound)
+		}
+		if n.FunctionPointerType != nil {
+			Walk(v, n.FunctionPointerType)
+		}
+
+	case *FunctionPointerTypeNode:
+		for _, param := range n.Parameters {
+			// Walk parameter children (Parameter is not a Node)
+			if param.Name != nil {
+				Walk(v, param.Name)
+			}
+			if param.Type != nil {
+				Walk(v, param.Type)
+			}
+			if param.DefaultValue != nil {
+				Walk(v, param.DefaultValue)
+			}
+		}
+		if n.ReturnType != nil {
+			Walk(v, n.ReturnType)
+		}
+
+	case *AddressOfExpression:
+		if n.Operator != nil {
+			Walk(v, n.Operator)
+		}
+		if n.Type != nil {
+			Walk(v, n.Type)
+		}
+
+	case *ArrayTypeNode:
+		if n.LowBound != nil {
+			Walk(v, n.LowBound)
+		}
+		if n.HighBound != nil {
+			Walk(v, n.HighBound)
+		}
+		if n.ElementType != nil {
+			Walk(v, n.ElementType)
+		}
+
+	case *SetTypeNode:
+		if n.ElementType != nil {
+			Walk(v, n.ElementType)
+		}
+	}
+}
+
+// Inspect traverses an AST in depth-first order, calling f for each node.
+// If f returns false, traversal of that node's children is skipped.
+// Otherwise, Inspect is called recursively for each child.
+//
+// This is a convenience wrapper around Walk for simple inspection tasks.
+func Inspect(node Node, f func(Node) bool) {
+	Walk(inspector(f), node)
+}
+
+// inspector is a helper type that implements Visitor for the Inspect function.
+type inspector func(Node) bool
+
+func (f inspector) Visit(node Node) Visitor {
+	if f(node) {
+		return f
+	}
+	return nil
+}
---
+++ pkg/ast/visitor_test.go
@@ -0,0 +1,436 @@
+package ast_test
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/cwbudde/go-dws/pkg/ast"
+	"github.com/cwbudde/go-dws/pkg/dwscript"
+)
+
+// TestWalk_VisitsAllNodes tests that Walk visits all nodes in the tree
+func TestWalk_VisitsAllNodes(t *testing.T) {
+	engine, _ := dwscript.New()
+	program, _ := engine.Parse(`
+		var x: Integer := 42;
+		var y: String := 'hello';
+
+		function Add(a, b: Integer): Integer;
+		begin
+			Result := a + b;
+		end;
+	`)
+
+	tree := program
+
+	// Count all visited nodes
+	nodeCount := 0
+	visitor := &countingVisitor{count: &nodeCount}
+	ast.Walk(visitor, tree)
+
+	// Should visit many nodes (program, statements, expressions, etc.)
+	if nodeCount == 0 {
+		t.Error("Walk() did not visit any nodes")
+	}
+
+	// We should visit at least: program, 3 statements (2 vars + 1 func),
+	// plus all their children
+	if nodeCount < 10 {
+		t.Errorf("Expected at least 10 nodes visited, got %d", nodeCount)
+	}
+}
+
+// TestWalk_VisitorReturnsNil tests that returning nil from Visit stops traversal
+func TestWalk_VisitorReturnsNil(t *testing.T) {
+	engine, _ := dwscript.New()
+	program, _ := engine.Parse(`
+		var x: Integer := 42;
+		function Test(): Integer;
+		begin
+			Result := 1;
+		end;
+	`)
+
+	tree := program
+
+	// Count only first-level nodes (stop after visiting root)
+	nodeCount := 0
+	visitor := &stopAfterFirstVisitor{count: &nodeCount}
+	ast.Walk(visitor, tree)
+
+	// Should only visit the root Program node
+	if nodeCount != 1 {
+		t.Errorf("Expected 1 node visited (root only), got %d", nodeCount)
+	}
+}
+
+// TestInspect_FindsFunctions tests using Inspect to find function declarations
+func TestInspect_FindsFunctions(t *testing.T) {
+	engine, _ := dwscript.New()
+	program, _ := engine.Parse(`
+		function Add(a, b: Integer): Integer;
+		begin
+			Result := a + b;
+		end;
+
+		function Multiply(a, b: Integer): Integer;
+		begin
+			Result := a * b;
+		end;
+	`)
+
+	tree := program
+
+	// Find all function declarations
+	functionNames := []string{}
+	ast.Inspect(tree, func(n ast.Node) bool {
+		if funcDecl, ok := n.(*ast.FunctionDecl); ok {
+			functionNames = append(functionNames, funcDecl.Name.Value)
+		}
+		return true // continue traversal
+	})
+
+	if len(functionNames) != 2 {
+		t.Errorf("Expected 2 functions, found %d", len(functionNames))
+	}
+
+	expected := map[string]bool{"Add": true, "Multiply": true}
+	for _, name := range functionNames {
+		if !expected[name] {
+			t.Errorf("Unexpected function name: %s", name)
+		}
+	}
+}
+
+// TestInspect_FindsVariables tests finding all variable declarations
+func TestInspect_FindsVariables(t *testing.T) {
+	engine, _ := dwscript.New()
+	program, _ := engine.Parse(`
+		var x: Integer := 1;
+		var y: String := 'test';
+		var z: Float := 3.14;
+	`)
+
+	tree := program
+
+	varCount := 0
+	ast.Inspect(tree, func(n ast.Node) bool {
+		if _, ok := n.(*ast.VarDeclStatement); ok {
+			varCount++
+		}
+		return true
+	})
+
+	if varCount != 3 {
+		t.Errorf("Expected 3 variables, found %d", varCount)
+	}
+}
+
+// TestInspect_StopsTraversal tests that returning false stops traversal
+func TestInspect_StopsTraversal(t *testing.T) {
+	engine, _ := dwscript.New()
+	program, _ := engine.Parse(`
+		var x: Integer := 42;
+		var y: String := 'hello';
+	`)
+
+	tree := program
+
+	visitCount := 0
+	ast.Inspect(tree, func(n ast.Node) bool {
+		visitCount++
+		// Stop after first node
+		return false
+	})
+
+	// Should only visit the root node
+	if visitCount != 1 {
+		t.Errorf("Expected 1 visit (stopped traversal), got %d", visitCount)
+	}
+}
+
+// TestInspect_NestedStructures tests traversing nested structures
+func TestInspect_NestedStructures(t *testing.T) {
+	engine, _ := dwscript.New()
+	program, _ := engine.Parse(`
+		function Test(): Integer;
+		begin
+			if x > 0 then
+				Result := 1
+			else
+				Result := 0;
+		end;
+	`)
+
+	tree := program
+
+	// Find if statement
+	foundIf := false
+	ast.Inspect(tree, func(n ast.Node) bool {
+		if _, ok := n.(*ast.IfStatement); ok {
+			foundIf = true
+		}
+		return true
+	})
+
+	if !foundIf {
+		t.Error("Did not find IfStatement in nested structure")
+	}
+}
+
+// TestWalk_AllNodeTypes tests that Walk handles all major node types
+func TestWalk_AllNodeTypes(t *testing.T) {
+	engine, _ := dwscript.New()
+
+	// Complex program with many node types
+	program, _ := engine.Parse(`
+		type TMyEnum = (Value1, Value2, Value3);
+
+		type TMyRecord = record
+			Field1: Integer;
+			Field2: String;
+		end;
+
+		class TMyClass
+			FValue: Integer;
+			function GetValue(): Integer;
+		end;
+
+		function TMyClass.GetValue(): Integer;
+		begin
+			Result := FValue;
+		end;
+
+		var arr: array of Integer;
+		var e: TMyEnum;
+
+		begin
+			arr := [1, 2, 3];
+			e := TMyEnum.Value1;
+
+			for var i := 0 to 10 do
+				PrintLn(i);
+
+			while x > 0 do
+				x := x - 1;
+
+			case e of
+				Value1: PrintLn('one');
+				Value2: PrintLn('two');
+			end;
+
+			try
+				DoSomething();
+			except
+				on E: Exception do
+					PrintLn('error');
+			end;
+		end
+	`)
+
+	tree := program
+
+	// Collect all node type names
+	nodeTypes := make(map[string]bool)
+	ast.Inspect(tree, func(n ast.Node) bool {
+		if n != nil {
+			nodeTypes[fmt.Sprintf("%T", n)] = true
+		}
+		return true
+	})
+
+	// Verify we encountered various node types
+	expectedTypes := []string{
+		"*ast.Program",
+		"*ast.EnumDecl",
+		"*ast.RecordDecl",
+		// Note: ClassDecl not included as parser doesn't fully support it yet
+		"*ast.FunctionDecl",
+		"*ast.VarDeclStatement",
+		"*ast.BlockStatement",
+		"*ast.ForStatement",
+		"*ast.WhileStatement",
+		"*ast.CaseStatement",
+		"*ast.TryStatement",
+		"*ast.ArrayLiteralExpression",
+		"*ast.CallExpression",
+	}
+
+	for _, expectedType := range expectedTypes {
+		if !nodeTypes[expectedType] {
+			t.Errorf("Did not encounter node type: %s", expectedType)
+		}
+	}
+}
+
+// countingVisitor counts all visited nodes
+type countingVisitor struct {
+	count *int
+}
+
+func (v *countingVisitor) Visit(node ast.Node) ast.Visitor {
+	if node != nil {
+		*v.count++
+	}
+	return v
+}
+
+// stopAfterFirstVisitor stops traversal after first node
+type stopAfterFirstVisitor struct {
+	count *int
+}
+
+func (v *stopAfterFirstVisitor) Visit(node ast.Node) ast.Visitor {
+	if node != nil {
+		*v.count++
+		return nil // stop traversal
+	}
+	return v
+}
+
+// Example_walk demonstrates using the Walk function with a custom visitor
+func Example_walk() {
+	engine, _ := dwscript.New()
+	program, _ := engine.Parse(`
+		function Add(a, b: Integer): Integer;
+		begin
+			Result := a + b;
+		end;
+	`)
+
+	tree := program
+
+	// Create a visitor that prints function names
+	visitor := &functionPrinter{}
+	ast.Walk(visitor, tree)
+
+	// Output:
+	// Function: Add
+}
+
+type functionPrinter struct{}
+
+func (v *functionPrinter) Visit(node ast.Node) ast.Visitor {
+	if node == nil {
+		return nil
+	}
+
+	if funcDecl, ok := node.(*ast.FunctionDecl); ok {
+		fmt.Printf("Function: %s\n", funcDecl.Name.Value)
+	}
+
+	return v
+}
+
+// Example_inspect demonstrates using Inspect to find specific nodes
+func Example_inspect() {
+	engine, _ := dwscript.New()
+	program, _ := engine.Parse(`
+		var x: Integer := 10;
+		var y: Integer := 20;
+		var z: String := 'hello';
+	`)
+
+	tree := program
+
+	// Find all variable declarations
+	varNames := []string{}
+	ast.Inspect(tree, func(n ast.Node) bool {
+		if varDecl, ok := n.(*ast.VarDeclStatement); ok {
+			for _, name := range varDecl.Names {
+				varNames = append(varNames, name.Value)
+			}
+		}
+		return true
+	})
+
+	fmt.Println("Variables:", strings.Join(varNames, ", "))
+
+	// Output:
+	// Variables: x, y, z
+}
+
+// Example_inspectStopTraversal shows how to stop traversal at specific nodes
+func Example_inspectStopTraversal() {
+	engine, _ := dwscript.New()
+	program, _ := engine.Parse(`
+		function Outer(): Integer;
+		begin
+			function Inner(): Integer;
+			begin
+				Result := 42;
+			end;
+			Result := Inner();
+		end;
+	`)
+
+	tree := program
+
+	// Find only top-level functions (don't descend into function bodies)
+	topLevelFunctions := []string{}
+	ast.Inspect(tree, func(n ast.Node) bool {
+		if funcDecl, ok := n.(*ast.FunctionDecl); ok {
+			topLevelFunctions = append(topLevelFunctions, funcDecl.Name.Value)
+			// Don't traverse into the function body (would find nested functions)
+			return false
+		}
+		return true
+	})
+
+	// Should only find 'Outer', not 'Inner'
+	fmt.Println("Top-level functions:", strings.Join(topLevelFunctions, ", "))
+
+	// Output:
+	// Top-level functions: Outer
+}
+
+// TestInspect_ComplexExpressions tests traversing complex expressions
+func TestInspect_ComplexExpressions(t *testing.T) {
+	engine, _ := dwscript.New()
+	program, _ := engine.Parse(`
+		var result: Integer := (a + b) * (c - d) / e;
+	`)
+
+	tree := program
+
+	// Count binary expressions
+	binaryCount := 0
+	ast.Inspect(tree, func(n ast.Node) bool {
+		if _, ok := n.(*ast.BinaryExpression); ok {
+			binaryCount++
+		}
+		return true
+	})
+
+	// Should have: +, *, -, / = 4 binary expressions
+	if binaryCount != 4 {
+		t.Errorf("Expected 4 binary expressions, found %d", binaryCount)
+	}
+}
+
+// TestWalk_WithNilNodes tests that Walk handles nil nodes gracefully
+func TestWalk_WithNilNodes(t *testing.T) {
+	// Create a simple program with proper Names field (slice, not single)
+	program := &ast.Program{
+		Statements: []ast.Statement{
+			&ast.VarDeclStatement{
+				Names: []*ast.Identifier{{Value: "x"}},
+				Value: nil, // Explicitly nil
+			},
+		},
+	}
+
+	// Should not panic with nil child nodes
+	nodeCount := 0
+	ast.Inspect(program, func(n ast.Node) bool {
+		if n != nil {
+			nodeCount++
+		}
+		return true
+	})
+
+	if nodeCount == 0 {
+		t.Error("Expected to visit some nodes")
+	}
+}
